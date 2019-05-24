e74cd1e2148a4623bfddcf342e04a8478dcfeff9
Marno <Marno.van-der-Maas@cl.cam.ac.uk>
2019-05-24 10:55:50 +0100

    Added back the make run command, which now calls runTests.sh. Also I made it so that runTests outputs the commands it executes.

--- benchmarks/runTests.sh
+++ benchmarks/runTests.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+set -x
 #caches specified with sets:ways:blocks
 spike --ic=16:1:8 --dc=16:1:8 --l2=128:1:8 -p1 --enclave=1 -m1024 --l2_partitioning=1 hello.out
 #spike --ic=16:1:8 --dc=16:1:8 --l2=128:1:8 -p1 --enclave=90 -m1024 --l2_partitioning=1 startenclaves.out
