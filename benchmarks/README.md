61e589efb827ca7093d9c293300489595711f25d
zatkh <zhtarkhani@gmail.com>
2019-07-14 19:27:17 -0700

    cleanup

---
+++ benchmarks/README.md
@@ -0,0 +1,15 @@
+to build:
+
+cd riscv-tools/riscv-isa-sim/benchmarks
+
+mkdir -p build
+
+make
+
+make run 
+
+or 
+
+make run_updated #can add make run SUFFIX=(a suffix to be added to the log files names)
+
+make test #only to build hello example
--- benchmarks/runTests_update.sh
+++ benchmarks/runTests_update.sh
@@ -2,6 +2,8 @@
 set -x
 make clean
 make all
+
+EXP_NUMBER=$1
 #caches specified with sets:ways:blocks
 OUTPUT_DIR=output
 mkdir -p $OUTPUT_DIR
@@ -10,12 +12,12 @@ CACHE_WAYS_AND_BLOCK_SIZE=:1:16
 INSTRUCTION_CACHE=16$CACHE_WAYS_AND_BLOCK_SIZE
 DATA_CACHE=$INSTRUCTION_CACHE
 SHARED_CACHE=256$CACHE_WAYS_AND_BLOCK_SIZE
-spike --ic=$INSTRUCTION_CACHE --dc=$DATA_CACHE --l2=$SHARED_CACHE -p1 --enclave=1 -m1024 --l2_partitioning=1 hello.out > $OUTPUT_DIR/hello.log
-spike --ic=$INSTRUCTION_CACHE --dc=$DATA_CACHE --l2=$SHARED_CACHE -p1 --enclave=14 -m1024 --l2_partitioning=1 -g startenclaves.out > $OUTPUT_DIR/startenclaves.log
+spike --ic=$INSTRUCTION_CACHE --dc=$DATA_CACHE --l2=$SHARED_CACHE -p1 --enclave=1 -m1024 --l2_partitioning=1 hello.out > $OUTPUT_DIR/hello_$EXP_NUMBER.log
+spike --ic=$INSTRUCTION_CACHE --dc=$DATA_CACHE --l2=$SHARED_CACHE -p1 --enclave=14 -m1024 --l2_partitioning=1 -g startenclaves.out > $OUTPUT_DIR/startenclaves_$EXP_NUMBER.log
 python3 tools/interprethistogram.py $OUTPUT_DIR/startenclaves.log $OUTPUT_DIR/startenclaveshistogram.csv
-spike --ic=$INSTRUCTION_CACHE --dc=$DATA_CACHE --l2=$SHARED_CACHE -p1 -m1024 --l2_partitioning=1 bitcount.out > $OUTPUT_DIR/bitcount.log
-spike --ic=$INSTRUCTION_CACHE --dc=$DATA_CACHE --l2=$SHARED_CACHE -p1 --enclave=1 -m1024 --l2_partitioning=1 bandwidth.out > $OUTPUT_DIR/bandwidth.log
-spike --ic=$INSTRUCTION_CACHE --dc=$DATA_CACHE --l2=$SHARED_CACHE -p1 --enclave=1 -m1024 --l2_partitioning=1 latency.out > $OUTPUT_DIR/latency.log
+spike --ic=$INSTRUCTION_CACHE --dc=$DATA_CACHE --l2=$SHARED_CACHE -p1 -m1024 --l2_partitioning=1 bitcount.out > $OUTPUT_DIR/bitcount_$EXP_NUMBER.log
+spike --ic=$INSTRUCTION_CACHE --dc=$DATA_CACHE --l2=$SHARED_CACHE -p1 --enclave=1 -m1024 --l2_partitioning=1 bandwidth.out > $OUTPUT_DIR/bandwidth_$EXP_NUMBER.log
+spike --ic=$INSTRUCTION_CACHE --dc=$DATA_CACHE --l2=$SHARED_CACHE -p1 --enclave=1 -m1024 --l2_partitioning=1 latency.out > $OUTPUT_DIR/latency_$EXP_NUMBER.log
 for PARTITIONING_SCHEME in {0..2}
 do
   for SETS_POWER in {6..12}
